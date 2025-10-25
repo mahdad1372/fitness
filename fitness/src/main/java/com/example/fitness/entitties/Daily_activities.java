4c31a722cc8ec7938034c8404d69885fc661fd4a
mahdad1372 <mnaderiniya@gmail.com>
2025-10-25 13:24:21 +0200

    adding the entity foods

--- fitness/src/main/java/com/example/fitness/entitties/Daily_activities.java
+++ fitness/src/main/java/com/example/fitness/entitties/Daily_activities.java
@@ -3,9 +3,6 @@ package com.example.fitness.entitties;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
-import java.math.BigDecimal;
-import java.text.DecimalFormat;
 import java.util.Date;
 
 
---
+++ fitness/src/main/java/com/example/fitness/entitties/Foods.java
@@ -0,0 +1,38 @@
+package com.example.fitness.entitties;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Table(name = "foods")
+@Entity
+public class Foods {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Integer food_id;
+    @Column(nullable = false)
+    private Integer user_id;
+    @Column(nullable = false)
+    private String food_name ;
+    @Column(nullable = false)
+    private String category;
+    @Column(nullable = false)
+    private Float calories;
+    @Column(nullable = false)
+    private Float protein;
+    @Column(nullable = false)
+    private Float carbohydrates;
+    @Column(nullable = false)
+    private Float fats;
+    @Column(nullable = false)
+    private String notes;
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+}
